b79a36462d7d4b50636a832b2a2d6a83133fef81
Ralf S. Engelschall <rse@engelschall.com>
2016-08-12 15:57:53 +0200

    place under version control

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# vue-params
-Vue plugin for global parameters triggering data-binding updates
+
+vue-params
+==========
+
+Vue plugin for global parameters triggering data-binding updates.
+
+<p/>
+<img src="https://nodei.co/npm/vue-params.png?downloads=true&stars=true" alt=""/>
+
+<p/>
+<img src="https://david-dm.org/rse/vue-params.png" alt=""/>
+
+About
+-----
+
+This is an plugin for the [Vue](http://vuejs.org) view data-binding
+library, adding the capability for global parameters under the namespace
+`Vue.params` which force data-binding updates of all currently active
+Vue instances.
+
+This is intended to be used for parameters which custom Vue
+methods are using and which should cause the data-binding to
+update (and this way the methods to get called) every time the
+parameter is changed. A typical example is the definition of
+the language parameter `Vue.params.i18nextLanguage` (of plugin
+[vue-i18next](https://github.com/rse/vue-i18next)) which causes all
+`$t("...")` method calls in Vue expressions to be re-executed.
+
+Usage
+-----
+
+```shell
+$ npm install vue
+$ npm install vue-params
+```
+
+```js
+Vue.use(VueParams)
+Vue.paramsCreate("foo")
+Vue.params.foo = "bar"
+Vue.paramsCreate("quux")
+Vue.params.quux = "baz"
+```
+
+License
+-------
+
+Copyright (c) 2016 Ralf S. Engelschall (http://engelschall.com/)
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ vue-params.js
@@ -0,0 +1,104 @@
+/*!
+**  vue-params -- Vue plugin for global parameters triggering data-binding updates
+**  Copyright (c) 2016 Ralf S. Engelschall <rse@engelschall.com>
+**
+**  Permission is hereby granted, free of charge, to any person obtaining
+**  a copy of this software and associated documentation files (the
+**  "Software"), to deal in the Software without restriction, including
+**  without limitation the rights to use, copy, modify, merge, publish,
+**  distribute, sublicense, and/or sell copies of the Software, and to
+**  permit persons to whom the Software is furnished to do so, subject to
+**  the following conditions:
+**
+**  The above copyright notice and this permission notice shall be included
+**  in all copies or substantial portions of the Software.
+**
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+**  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+**  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+**  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+**  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+**  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+**  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/*  Universal Module Definition (UMD)  */
+(function (root, name, factory) {
+    /* global define: false */
+    /* global module: false */
+    if (typeof define === "function" && typeof define.amd !== "undefined")
+        /*  AMD environment  */
+        define(name, function () { return factory(root); })
+    else if (typeof module === "object" && typeof module.exports === "object")
+        /*  CommonJS environment  */
+        module.exports = factory(root)
+    else
+        /*  Browser environment  */
+        root[name] = factory(root)
+}(this, "VueParams", function (/* root */) {
+    /*  the Plugin */
+    var VueParams = {
+        install: function (Vue /*, options */) {
+            /*  define observable "Vue.params" object  */
+            var paramValue   = {}
+            var paramWatcher = []
+            Object.defineProperty(Vue, "params", {
+                enumerable:   true,
+                configurable: false,
+                value:        {},
+                writeable:    false
+            })
+
+            /*  hook into the Vue instance life-cycle  */
+            var _init    = Vue.prototype._init
+            var _destroy = Vue.prototype._destroy
+            Vue.prototype._init = function () {
+                paramWatcher.push(this)
+                return _init.apply(this, arguments)
+            }
+            Vue.prototype._destroy = function () {
+                paramWatcher = paramWatcher.filter((watcher) => watcher !== this)
+                return _destroy.apply(this, arguments)
+            }
+
+            /*  provide global API methods  */
+            Vue.paramsCreate = function (params) {
+                if (typeof params === "string")
+                    params = [ params ]
+                if (!(typeof params === "object" && params instanceof Array))
+                    throw new Error("invalid params parameter (expected single string or array of strings)")
+                params.forEach(function (name) {
+                    Object.defineProperty(Vue.params, name, {
+                        enumerable:   true,
+                        configurable: true,
+                        get: function () {
+                            return paramValue[name]
+                        },
+                        set: function (val) {
+                            if (paramValue[name] !== val) {
+                                paramValue[name] = val
+                                paramWatcher.forEach((vue) => {
+                                    vue.$forceUpdate()
+                                })
+                            }
+                        }
+                    })
+                })
+            }
+            Vue.paramsDestroy = function (params) {
+                if (typeof params === "string")
+                    params = [ params ]
+                if (!(typeof params === "object" && params instanceof Array))
+                    throw new Error("invalid params parameter (expected single string or array of strings)")
+                params.forEach(function (name) {
+                    delete Vue.params[name]
+                    delete paramValue[name]
+                })
+            }
+        }
+    }
+
+    /*  export API  */
+    return VueParams
+}))
+
